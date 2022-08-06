d7eecee9809f447e10e763bfcde6540288c9fc95
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-08-06 17:58:47 +0700

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,17 +1,100 @@
-class Message {
-  constructor(options = {}, client) {
-    this.client = client
-    if(client?.advmode) {
-      //Maintenance
-    } else {
-      this.guildId = options?.message?.serverId
-      this.authorId = options?.message?.userId
-      this.channelId = options?.message?.channelId
-      this.id = options?.message?.id
-      this.content = options?.message?.content
-      this.embeds = options?.message?.embeds
+//========== STRUCTURE DATA
+const Constants = require("./../util/constants.js")
+
+const UserClient = require("./../structure/UserClient.js")
+const Message = require("./../structure/Message.js")
+//========== PACKAGE
+const { EventEmitter } = require("node:events")
+const axios = require('axios')
+const WebSocket = require("ws");
+
+//========= CLASS
+class Client extends EventEmitter {
+  constructor(options = {}) {
+    super()
+
+    this.token = options?.token || null;
+    this.advmode = options?.advance || false
+  }
+
+  login(token) {
+    if (this.token === null) {
+      if (!token) throw new Error("Token Tidak Ada")
     }
+    this.startWebsocket()
+  }
+
+  destroy() {
+    return this.ws.destroy()
+  }
+
+  startWebsocket() {
+    let wssurl = 'wss://api.guilded.gg/v1/websocket'
+
+    this.ws = new WebSocket(wssurl, {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      },
+    });
+
+    this.ws.onopen = (data) => {
+      console.log('Lumine.js Succesfull To Connect Websocket');
+    }
+    this.ws.onclose = this.ws.onerror = (e) => {
+      this.ws = null
+      console.log('Reconnect...')
+      this.startWebsocket()
+    }
+
+    var OPCodes = {
+      WELLCOME: 1
+    }
+
+    this.ws.onmessage = ({ data }) => {
+      let packet = JSON.parse(data)
+      switch (packet.op) {
+        case OPCodes.WELLCOME:
+          this.emit("ready", new UserClient(packet.d, this))
+          const packg = require("./../../package.json")
+          console.log(`====== Lumine.js (Project)\n${packg.name} - ${packg.version}\n\nNow Login To ${new UserClient(packet.d, this).username}\n======`)
+          setInterval(function() {
+            this.ws.ping()
+          }.bind(this), packet.d.heartbeatIntervalMs - 3000)
+          break;
+      }
+
+      console.log(packet)
+
+      switch (packet.t) {
+        case "ChatMessageCreated":
+          this.emit("messageCreate", new Message(packet.d, this))
+          break;
+      }
+
+    };
+  }
+
+  requestAPI(method = "", params = "", data) {
+    let object = {
+      method: method,
+      url: "https://www.guilded.gg/api/v1" + params,
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    }
+
+    if (data) object.data = data
+
+    return axios(object).then(x => "").catch(err => {
+      console.log(err)
+    })
+  }
+
+  sendMessage(channelId, data) {
+    if (!channelId) return new TypeError("Uknown Channel Id")
+    if ((!data?.content) || (!data?.embeds)) return new TypeError("Cannot Send Empty Message")
+    this.requestAPI("POST", Constantd.ENDPOINTS.MESSAGE)
   }
 }
 
-module.exports = Message
+module.exports = Client
