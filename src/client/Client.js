81d95b79f0352b9cad2dd531dd531855506edea1
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-08-06 17:52:49 +0700

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -6,24 +6,7 @@ class Message {
     } else {
       this.guildId = options?.message?.serverId
       this.authorId = options?.message?.userId
-      this.channelId
-      this.id = options?.message?.id
-      this.content = options?.message?.content
-      this.embeds = options?.message?.embeds
-    }
-  }
-}
-
-module.exports = Message
-class Message {
-  constructor(options = {}, client) {
-    this.client = client
-    if(this.client.advmode) {
-      //Maintenance
-    } else {
-      this.guildId = options?.message?.serverId
-      this.authorId = options?.message?.userId
-      this.channelId
+      this.channelId = options?.message?.channelId
       this.id = options?.message?.id
       this.content = options?.message?.content
       this.embeds = options?.message?.embeds
