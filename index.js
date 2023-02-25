fdf93410f41a4845450bbc47f2c3c90dab952aaa
tgigroupid@gmail.com <tgigroupid@gmail.com>
2023-02-25 18:50:46 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Client = require("./src/client/Client.js")
 const Collection = require('./src/structure/Embed.js')
-const Collection = require('./src/util/Collection.js')
 const Constants = require('./src/util/Constants.js')
 module.exports = {
   Client: Client,
